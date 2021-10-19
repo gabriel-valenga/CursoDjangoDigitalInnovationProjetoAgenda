301ecd4fcea37d2ed5cd70c4efe6173097b664c1
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2021-10-19 11:16:05 -0300

    Criada pagina de listagem de eventos

--- agenda/core/views.py
+++ agenda/core/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from core.models import Evento
 
 
 def local_evento(request, titulo_evento):
     evento = Evento.objects.get(titulo=titulo_evento)
     return HttpResponse(evento.local)
+
+
+def lista_eventos(request):
+    usuario = request.user
+    eventos = Evento.objects.all()
+    dados = {'eventos':eventos}
+    return render(request, 'agenda.html', dados)

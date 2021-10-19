af66e4db2ebe5a23a0dce23c88f5ba8f932144b2
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2021-10-19 15:26:17 -0300

    Incluida funcao e pagina de autenticacao

--- agenda/core/views.py
+++ agenda/core/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, HttpResponse, redirect
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 def local_evento(request, titulo_evento):
@@ -7,8 +10,31 @@ def local_evento(request, titulo_evento):
     return HttpResponse(evento.local)
 
 
+@login_required(login_url='/login/')
 def lista_eventos(request):
     usuario = request.user
-    eventos = Evento.objects.all()
-    dados = {'eventos':eventos}
+    eventos = Evento.objects.filter(usuario=usuario)
+    dados = {'eventos': eventos}
     return render(request, 'agenda.html', dados)
+
+
+def login_user(request):
+    return render(request, 'login.html')
+
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, 'Usuário ou senha inválido!')
+        return redirect('/')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')

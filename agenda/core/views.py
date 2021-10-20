fa32ec1b003791d29c1a900497e21a5248b425c1
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2021-10-20 16:30:37 -0300

    Adicionado retorno de lista por json, retorno de http 404 e configurado time zone para pt-br

--- agenda/core/views.py
+++ agenda/core/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, HttpResponse, redirect
-from core.models import Evento
+from core.models import Evento, User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from datetime import datetime, timedelta
+from django.http.response import Http404, JsonResponse
 
 
 def local_evento(request, titulo_evento):
@@ -13,7 +15,9 @@ def local_evento(request, titulo_evento):
 @login_required(login_url='/login/')
 def lista_eventos(request):
     usuario = request.user
-    eventos = Evento.objects.filter(usuario=usuario)
+    data_atual = datetime.now() - timedelta(hours=1)
+    eventos = Evento.objects.filter(usuario=usuario,
+                                    data__gt=data_atual)
     dados = {'eventos': eventos}
     return render(request, 'agenda.html', dados)
 
@@ -79,7 +83,19 @@ def submit_evento(request):
 @login_required(login_url='/login/')
 def delete_evento(request, id_evento):
     usuario = request.user
-    evento_ = Evento.objects.get(id=id_evento)
+    try:
+        evento_ = Evento.objects.get(id=id_evento)
+    except Exception:
+        raise Http404()
     if usuario == evento_.usuario:
         evento_.delete()
+    else:
+        raise Http404()
     return redirect('/')
+
+
+@login_required(login_url='/login/')
+def json_lista_evento(request, id_usuario):
+    usuario = User.objects.get(id=id_usuario)
+    evento_ = Evento.objects.filter(usuario=usuario).values('id', 'titulo')
+    return JsonResponse(list(evento_), safe=False)
